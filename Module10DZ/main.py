5f5fbdfb359f688d12ac98f8bd3393c82599264b
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2024-09-12 17:23:16 +0700

    Задание 2
    Сделал задание про книги

--- Module10DZ/main.py
+++ Module10DZ/main.py
@@ -3,83 +3,175 @@
 # в полях класса: название модели, год выпуска, производителя, объем двигателя, цвет машины, цену. Реализуйте
 # методы класса для ввода данных, вывода данных, реализуйте доступ к отдельным полям через методы класса.
 
+#
+# class Car:
+#
+#
+# 	def __init__(self):
+# 		with open('cars.txt', 'r', encoding='utf-8') as f:
+# 			# lines = f.readlines()
+# 			self.brand: str = input('Введите название производителя: ')
+# 			self.model = input('Введите название модели: ')
+# 			self.year = input('Введите год выпуска авто: ')
+# 			self.engine_capacity = input('Введите объем двигателя: ')
+# 			self.color = input('Введите цвет машины: ')
+# 			self.price = input('Введите стоимость машины: ')
+#
+# 	def save_cars_info(self):
+# 		with open('cars.txt', 'a', encoding='utf-8') as f:
+# 			f.write(f'{self.brand},{self.model},{self.year},{self.engine_capacity},{self.color},{self.price}\n')
+#
+#
+# 	def show_car_info(self):
+# 		print(f'Название производителя - {self.brand}')
+# 		print(f'Название модели - {self.model}')
+# 		print(f'Год выпуска авто - {self.year}')
+# 		print(f'Объем двигателя - {self.engine_capacity}')
+# 		print(f'Цвет машины - {self.color}')
+# 		print(f'Цена машины - {self.price} рублей')
+#
+# 	def show_brand(self):
+# 		with open('cars.txt', 'r', encoding='utf-8') as f:
+# 			lines = f.readlines()
+# 			for line in lines:
+# 				parts = line.split(',')
+# 				print(f'Название производителей в базе - {parts[0].strip()}')
+#
+#
+# 	def show_model(self):
+# 		with open('cars.txt', 'r', encoding='utf-8') as f:
+# 			lines = f.readlines()
+# 			for line in lines:
+# 				parts = line.split(',')
+# 				print(f'Название моделей в базе - {parts[2].strip()}')
+#
+#
+# 	def show_year(self):
+# 		with open('cars.txt', 'r', encoding='utf-8') as f:
+# 			lines = f.readlines()
+# 			for line in lines:
+# 				parts = line.split(',')
+# 				print(f'Год выпуска авто базе - {parts[3].strip()}')
+#
+#
+# 	def show_engine(self):
+# 		with open('cars.txt', 'r', encoding='utf-8') as f:
+# 			lines = f.readlines()
+# 			for line in lines:
+# 				parts = line.split(',')
+# 				print(f'Объем двигателя в базе - {parts[4].strip()}')
+#
+#
+# 	def show_color(self):
+# 		with open('cars.txt', 'r', encoding='utf-8') as f:
+# 			lines = f.readlines()
+# 			for line in lines:
+# 				parts = line.split(',')
+# 				print(f'Цвет авто в базе - {parts[5].strip()}')
+#
+#
+# 	def show_price(self):
+# 		with open('cars.txt', 'r', encoding='utf-8') as f:
+# 			lines = f.readlines()
+# 			for line in lines:
+# 				parts = line.split(',')
+# 				print(f'Стоимость авто в базе - {parts[6].strip()}')
+#
+# a = Car()
+# a.save_cars_info()
+# a.show_car_info()
+# a.show_brand()
 
-class Car:
+
+# Задание 2
+# Реализуйте класс «Книга». Необходимо хранить в
+# полях класса: название книги, год выпуска, издателя,
+# жанр, автора, цену. Реализуйте методы класса для ввода
+# данных, вывода данных, реализуйте доступ к отдельным
+# полям через методы класса.
+
+
+class Book:
 
 
 	def __init__(self):
-		with open('cars.txt', 'r', encoding='utf-8') as f:
+		with open('book.txt', 'r', encoding='utf-8') as f:
 			# lines = f.readlines()
-			self.brand: str = input('Введите название производителя: ')
-			self.model = input('Введите название модели: ')
-			self.year = input('Введите год выпуска авто: ')
-			self.engine_capacity = input('Введите объем двигателя: ')
-			self.color = input('Введите цвет машины: ')
-			self.price = input('Введите стоимость машины: ')
+			self.name: str = input('Введите название книги: ')
+			self.year = input('Введите год выпуска книги: ')
+			self.publisher = input('Введите издателя: ')
+			self.author = input('Введите автора: ')
+			self.genre = input('Введите жанр: ')
+			self.price = input('Введите стоимость книги: ')
 
-	def save_cars_info(self):
-		with open('cars.txt', 'a', encoding='utf-8') as f:
-			f.write(f'{self.brand},{self.model},{self.year},{self.engine_capacity},{self.color},{self.price}\n')
+	def save_book_info(self):
+		with open('book.txt', 'a', encoding='utf-8') as f:
+			f.write(f'{self.name},{self.year},{self.publisher},{self.author},{self.genre},{self.price}\n')
 
 
-	def show_car_info(self):
-		print(f'Название производителя - {self.brand}')
-		print(f'Название модели - {self.model}')
-		print(f'Год выпуска авто - {self.year}')
-		print(f'Объем двигателя - {self.engine_capacity}')
-		print(f'Цвет машины - {self.color}')
-		print(f'Цена машины - {self.price} рублей')
+	def show_book_info(self):
+		print(f'Название книги - {self.name}')
+		print(f'Год выпуска - {self.year}')
+		print(f'Издатель - {self.publisher}')
+		print(f'Автор - {self.author}')
+		print(f'Жанр - {self.genre}')
+		print(f'Цена - {self.price} рублей')
 
-	def show_brand(self):
-		with open('cars.txt', 'r', encoding='utf-8') as f:
+	def show_name(self):
+		with open('book.txt', 'r', encoding='utf-8') as f:
 			lines = f.readlines()
 			for line in lines:
 				parts = line.split(',')
-				print(f'Название производителей в базе - {parts[0].strip()}')
+				print(f'Название книг в базе - {parts[0].strip()}')
 
 
-	def show_model(self):
-		with open('cars.txt', 'r', encoding='utf-8') as f:
+	def show_publisher(self):
+		with open('book.txt', 'r', encoding='utf-8') as f:
 			lines = f.readlines()
 			for line in lines:
 				parts = line.split(',')
-				print(f'Название моделей в базе - {parts[2].strip()}')
+				print(f'Название издателей в базе - {parts[3].strip()}')
 
 
 	def show_year(self):
-		with open('cars.txt', 'r', encoding='utf-8') as f:
+		with open('book.txt', 'r', encoding='utf-8') as f:
 			lines = f.readlines()
 			for line in lines:
 				parts = line.split(',')
-				print(f'Год выпуска авто базе - {parts[3].strip()}')
+				print(f'Год выпуска книги в базе - {parts[2].strip()}')
 
 
-	def show_engine(self):
-		with open('cars.txt', 'r', encoding='utf-8') as f:
+	def show_author(self):
+		with open('book.txt', 'r', encoding='utf-8') as f:
 			lines = f.readlines()
 			for line in lines:
 				parts = line.split(',')
-				print(f'Объем двигателя в базе - {parts[4].strip()}')
+				print(f'Автор книги в базе - {parts[4].strip()}')
 
 
-	def show_color(self):
-		with open('cars.txt', 'r', encoding='utf-8') as f:
+	def show_genre(self):
+		with open('book.txt', 'r', encoding='utf-8') as f:
 			lines = f.readlines()
 			for line in lines:
 				parts = line.split(',')
-				print(f'Цвет авто в базе - {parts[5].strip()}')
+				print(f'Жанр книги в базе - {parts[5].strip()}')
 
 
 	def show_price(self):
-		with open('cars.txt', 'r', encoding='utf-8') as f:
+		with open('book.txt', 'r', encoding='utf-8') as f:
 			lines = f.readlines()
 			for line in lines:
 				parts = line.split(',')
-				print(f'Стоимость авто в базе - {parts[6].strip()}')
+				print(f'Стоимость книги в базе - {parts[6].strip()}')
 
-a = Car()
-a.save_cars_info()
-a.show_car_info()
-a.show_brand()
+a = Book()
+a.save_book_info()
+a.show_book_info()
+a.show_name()
+a.show_year()
+a.show_publisher()
+a.show_author()
+a.show_genre()
+a.show_price()
 
 
