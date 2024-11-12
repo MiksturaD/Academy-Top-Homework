55361493707163038209de91d9b9cd2f6c9fc1a4
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2024-11-12 17:57:02 +0700

    12.11.2024 Update

--- Django0711124/authors/urls.py
+++ Django0711124/authors/urls.py
@@ -4,6 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name="main"),
-    path('authors', views.authors, name='authors'),
+    path('authors', views.author, name='authors'),
     path('top_book', views.top_book, name="top_book"),
 ]
