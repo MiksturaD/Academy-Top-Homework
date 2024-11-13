c00cafe80ffd607fc8d848e2185ec645ad15253e
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2024-11-13 17:19:13 +0700

    Update.13.11.24_17:20

--- Django0711124/authors/views.py
+++ Django0711124/authors/views.py
@@ -1,13 +1,20 @@
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render, get_object_or_404
-from .models import Author, TopBooks
+from authors.models import Author, TopBooks
 def index(request):
     authors = Author.objects.all()
-    return render(request, 'index.html', {'author':author})
-
-def author(request, author_id=None):
-    author = Author.objects.get(Author, pk=author_id)  # Получаем автора по ID
-    return render(request, 'authors.html', {'author':author})
+    top_books = TopBooks.objects.all()
+    return render(request, 'index.html', {
+        'authors': authors,
+        'top_books': top_books,
+    })
+def author(request, author_id):
+    author = Author.objects.all()
+    top_books = TopBooks.objects.filter(author_id=author_id)
+    return render(request, 'detail.html', {
+        'author': author,
+        'top_books': top_books
+    })
 
 def top_book(request):
     top_books = TopBooks.objects.all()  # Получаем все топ-книги
@@ -15,5 +22,5 @@ def top_book(request):
 
 def detail(request, author_id=None, top_book_id=None):
     author = get_object_or_404(Author, pk=author_id)  # Получаем автора по ID
-    top_book = get_object_or_404(TopBooks, pk=top_book_id)  # Получаем топ-книгу по ID
+    top_book = TopBooks.objects.filter(author_id=author_id)  # Получаем топ-книгу по ID
     return render(request, 'detail.html',{'author':author, 'top_book':top_book})
