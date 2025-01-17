7cbe73353a1888cc8bb07414487f859e70cced02
MiksturaD <162081822+MiksturaD@users.noreply.github.com>
2025-01-17 15:18:31 +0700

    Админка и русификация

--- Internet_shop/store/admin.py
+++ Internet_shop/store/admin.py
@@ -1,3 +1,4 @@
+
 from django.contrib import admin
 
 from store.models import Category, Product, Order, User
@@ -6,10 +7,25 @@ admin.site.site_header = "Интернет магазин по продаже HI
 admin.site.site_title = "Админка магазина"
 admin.site.index_title = "Добро пожаловать в магазин"
 
-admin.site.register(Category)
-admin.site.register(Product)
-admin.site.register(Order)
-admin.site.register(User)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'price', 'is_available', 'category')
+    list_filter = ('name', 'description', 'price', 'is_available', 'category')  # Фильтры в правой части админки
+    search_fields = ('name', )  # Поля для поиска
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'customer_email', 'order_date')
+    list_filter = ('customer_name', 'order_date')
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('first_name', 'last_name')
+    list_filter = ('first_name', 'last_name')
 
 
 
--- Internet_shop/store/models.py
+++ Internet_shop/store/models.py
@@ -2,25 +2,34 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 class Category(models.Model):
-  name = models.CharField(max_length=100)
+  class Meta:
+    verbose_name = 'Категория'
+    verbose_name_plural = 'Категории'
+  name = models.CharField(max_length=100, verbose_name='Название категории')
 
   def __str__(self):
     return self.name
 
 class Product(models.Model):
-  name = models.CharField(max_length=100)
-  description = models.TextField()
-  price = models.DecimalField(max_digits=10, decimal_places=2)
-  is_available = models.BooleanField(default=True)
-  category = models.ForeignKey(Category, on_delete=models.CASCADE)
+  class Meta:
+    verbose_name = 'Товар'
+    verbose_name_plural = 'Товары'
+  name = models.CharField(max_length=100, verbose_name='Название')
+  description = models.TextField(verbose_name='Описание')
+  price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='Цена')
+  is_available = models.BooleanField(default=True, verbose_name='Доступен ли к заказу')
+  category = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name='Категория')
 
   def __str__(self):
     return f'{self.name}, {self.description}, {self.price}, {self.is_available}, {self.category}'
 
 class Order(models.Model):
-  customer_name = models.CharField(max_length=100)
-  customer_email = models.EmailField()
-  order_date = models.DateTimeField()
+  class Meta:
+    verbose_name = 'Заказ'
+    verbose_name_plural = 'Заказы'
+  customer_name = models.CharField(max_length=100, verbose_name='Имя покупателя')
+  customer_email = models.EmailField(verbose_name='Емайл')
+  order_date = models.DateTimeField(verbose_name='Дата заказа')
   line = models.ManyToManyField(Product)
 
 
@@ -29,8 +38,11 @@ class Order(models.Model):
 
 
 class User(AbstractUser):
-  first_name = models.CharField(max_length=100)
-  last_name = models.CharField(max_length=100)
+  class Meta:
+    verbose_name = 'Пользователь'
+    verbose_name_plural = 'Пользователи'
+  first_name = models.CharField(max_length=100, verbose_name='Имя')
+  last_name = models.CharField(max_length=100, verbose_name='Фамилия')
 
   def __str__(self):
     return self.username
